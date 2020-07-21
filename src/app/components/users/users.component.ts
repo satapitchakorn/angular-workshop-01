177271214e4813b2bf241876d2d5b0be4db2bae0
Satapitchakorn Punjarat <satapitchakorn.punjarat@allianz.com>
2020-07-21 12:11:30 +0700

    Workshop-04.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -7,38 +7,36 @@ import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@ang
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  submitted = false;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
 
   }
+  formArray: FormArray = this.fb.array([])
   form: FormGroup = this.fb.group({
-    firstname: new FormControl(null, Validators.required),
-    lastname: new FormControl(''),
-    name: new FormControl(null),
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    name: new FormControl(''),
     isCheck: new FormControl(false)
   })
-  list: Array<FormGroup> = []
   count: number = 0;
   submit() {
+    this.submitted = true
     if (this.form.valid) {
+      this.submitted = false
       this.form.value.name = this.form.value.firstname + ' ' + this.form.value.lastname
-      this.list.push(this.form)
+      this.formArray.push(this.form)
       this.form = this.fb.group({
-        firstname: new FormControl(null, Validators.required),
-        lastname: new FormControl(''),
-        name: new FormControl(null),
+        firstname: new FormControl('', Validators.required),
+        lastname: new FormControl('', Validators.required),
+        name: new FormControl(''),
         isCheck: new FormControl(false)
       })
     }
   }
-  actionAt(item) {
+  actionAt(item: FormGroup) {
     item.value.isCheck = !item.value.isCheck
-    if (item.value.isCheck) {
-      this.count++
-    } else {
-      this.count--
-    }
+    this.count = this.formArray.controls.filter((x: FormGroup) => x.controls.isCheck.value).length;
   }
 }
